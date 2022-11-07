a492c75b0e3340b46bb2dd01b4631d4fbc2236ff
charles14700 <mwanikicharles14700@gmail.com>
2022-11-07 06:05:55 -0800

    first commit

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='fixed top-0 left-0 right-0 bg-red-400 h-20'>
 
         <div className='relative top-3'>
-            <Image src="/incoglogo.png " width={70} height={60} alt="image"/>
+            <Image src="/incoglogo.png" width={70} height={60} alt="image"/>
         </div>
     </div>
   )
