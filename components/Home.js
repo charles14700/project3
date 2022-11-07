c2af2ae0a7d16e5b7973508f69c10a07de1fabeb
charles14700 <mwanikicharles14700@gmail.com>
2022-11-07 05:58:54 -0800

    first commit

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import Shipping from "./shipping";
+import Shipping from "./Shipping";
 const Home = () => {
   return (
     <div className="h-screen w-full bg-blue-500">
