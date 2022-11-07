e43299ec2e9f0920bde0152e8152f5d662e79c78
charles14700 <mwanikicharles14700@gmail.com>
2022-11-07 05:03:35 -0800

    first commit

--- components/Home.js
+++ components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="h-screen w-full bg-blue-500">
       <div className="relative top-[5rem] flex justify-center">
-        <Image src="/incog.png" width={100} height={100} />
+        <Image src="/incog.png" width={100} height={100} alt="image"/>
       </div>
       <div className="relative top-[5rem] flex justify-center text-black font-bold">
         <h3>Lets pay with paypal</h3>
